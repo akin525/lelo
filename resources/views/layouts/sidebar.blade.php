d37e8ffd66fec193340051d0588970def55e9d3c
IT <akinlabisamon15@gmail.com>
2022-05-04 21:03:52 +0000

    29/04/2022

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -137,14 +137,15 @@
             <div class="main-profile">
                 <div class="image-bx">
                     <img src="{{asset('images/avater.jpg')}}" alt="">
+                    <a class="nav-link bell dz-theme-mode" href="#">
+                        <i id="icon-light" class="fa fa-sun-o"></i>
+                        <i id="icon-dark" class="fa fa-moon-o"></i>
+                    </a>
                     <a href="javascript:void(0);"><i class="fa fa-cog" aria-hidden="true"></i></a>
                 </div>
                 <h5 class="name"><span class="font-w400">Hello,</span> {{ Auth::user()->username }}</h5>
                 <p class="email">{{ Auth::user()->email }}</p>
-                <a class="nav-link bell dz-theme-mode" href="#">
-                    <i id="icon-light" class="fa fa-sun-o"></i>
-                    <i id="icon-dark" class="fa fa-moon-o"></i>
-                </a>
+
             </div>
             <ul class="metismenu" >
                 <li><a href="{{url('welcome')}}" class="ai-icon">
