796781b0054c0cfb4eef70966b960bddf3ef419e
IMACULATE <odejinmiemmanuel@gmail.com>
2022-07-12 16:52:55 +0100

    12/07/2022

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -239,7 +239,7 @@
         }
     </style>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <a href="http:wa.me/2348036711447" class="float" target="_blank">
+    <a href="http:wa.me/2348036711447/?text=Goodday, My Username is....." class="float" target="_blank">
         <i class="fa fa-whatsapp my-float"></i>
     </a>
 
